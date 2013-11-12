c40326f9d4f556412665dacada42127c6604b526
Brent Danley <brent@brentdanley.com>
2013-11-12 00:29:16 +0000

    Commit prior to adding to GitHub.

---
+++ wp-content/themes/jq-custom/searchform.php
@@ -0,0 +1,6 @@
+<!-- custom search form -->
+<div id="searchform" class="clearfix">
+	<form method="get" action="<?php bloginfo('url'); ?>/">
+		<input type="search" name="search" id="search" value="" placeholder="Search..." /><input type="submit" id="search-submit" class="submit" name="submit" value="Go" />
+	</form>
+</div>
