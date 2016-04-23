aca17e9179b760e7c4c1fc94a5f3f2368d1da5cf
Brent Danley <brent@brentdanley.com>
2016-04-23 15:23:12 +0000

    Adds new theme '_brentdanley'.

---
+++ wp-content/themes/_brentdanley/inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.com/
+ *
+ * @package _brentdanley
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function _brentdanley_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => '_brentdanley_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', '_brentdanley_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function _brentdanley_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+		    get_template_part( 'template-parts/content', 'search' );
+		else :
+		    get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
