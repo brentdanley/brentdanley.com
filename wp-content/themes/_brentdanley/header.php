2800434079c672c293e6371cc61732c315f0a17e
Brent Danley <brentdanley@gmail.com>
2016-04-30 11:44:53 -0400

    Makes site title include description in header.

--- wp-content/themes/_brentdanley/header.php
+++ wp-content/themes/_brentdanley/header.php
@@ -28,17 +28,22 @@
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> | <?php echo getSiteDescription(); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> | <?php echo getSiteDescription(); ?></a></p>
 			<?php
 			endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
+			function getSiteDescription()
+			{
+				$description = get_bloginfo('description', 'display');
+				if ($description || is_customize_preview()) :
+					return '<span class="site-description">' . $description /* WPCS: xss ok. */ . '</span>';
+				endif;
+
+				return false;
+			}
+			?>
 		</div><!-- .site-branding -->
 	</header><!-- #masthead -->
 
